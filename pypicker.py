167ebf5ba63b04c6fe42cbab9b88093ddc3a1abf
JasonBristol <gethsemane369@gmail.com>
2014-04-24 11:45:57 -0400

    Fixed #3

--- pypicker.py
+++ pypicker.py
@@ -12,20 +12,17 @@ parser.add_argument('-o', help='write results to an output file')
 args = parser.parse_args()
 
 options = []
-output = 0
 
 if args.i != None:
 	options = [line.strip() for line in open(args.i)]
 
-if args.o != None:
-	output = 1
-
 if args.n > 0 and args.n <= len(options):
-	if output == 0:
+	if args.o != None:
+		with open('out.txt','a') as f:
+			for i in random.sample(options, args.n): f.write(i + '\n')
+	else:
 		print "Picking " + repr(args.n) + " individuals out of " + repr(len(options)) + ":"
 		for i in random.sample(options, args.n): print i
-	else:
-		with open('out.txt','a') as f: for i in random.sample(options, args.n): f.write(i + '\n')
 else:
 	print "n out of bounds, please select a number within 1 < n < total items"
 
