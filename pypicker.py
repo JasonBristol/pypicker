9077bd46c3d0d940b0e0b74db460f83f68f901c9
Jason Bristol <gethsemane369@gmail.com>
2014-08-22 12:08:20 -0400

    Code cleanup, added new arguments (unimplemented)

--- pypicker.py
+++ pypicker.py
@@ -2,11 +2,14 @@ import argparse, random, sys
 from random import choice
 
 parser = argparse.ArgumentParser(
-	description='Randomly pick n items from a list',
+	description='Randomly pick n items from a list', 
 	epilog='Created by Jason Bristol <Gethsemane369@gmail.com>')
-parser.add_argument('-n', type=int, default=1, help='number of items to pick (default 1)')
-parser.add_argument('-i', help='input file of items to choose from')
-parser.add_argument('-o', help='output file to write results to')
+
+parser.add_argument('num', metavar='N', type=int,     default=1, help='number of items to pick (default 1)')
+parser.add_argument('-ng',              type=int,     default=1, help='number of result groups to generate (default 1)')
+parser.add_argument('-i',               type=str,                help='input file of items to choose from')
+parser.add_argument('-o',               type=str,                help='output file to write results to')
+parser.add_argument('-u',               action='store_true',     help='force unique results')
 
 args = parser.parse_args()
 
@@ -15,14 +18,15 @@ options = []
 if args.i != None:
 	options = [line.strip() for line in open(args.i)]
 
-if args.n > 0 and args.n <= len(options):
-	if args.o != None:
-		with open('out.txt','a') as f:
-			for i in random.sample(options, args.n): f.write(i + '\n')
-	else:
-		print "Picking " + repr(args.n) + " individuals out of " + repr(len(options)) + ":"
-		for i in random.sample(options, args.n): print i
-else:
-	print "n out of bounds, please select a number within 1 < n < total items"
+if not (args.num > 0 and args.num <= len(options)):
+	print "\nn out of bounds, please select a number within 1 < n < total items\n"
+	sys.exit(1)
 
-sys.exit()
+if args.o != None:
+	with open('out.txt','a') as f:
+		for i in random.sample(options, args.num): f.write(i + '\n')
+else:
+	print "Picking " + repr(args.num) + " individuals out of " + repr(len(options)) + ":"
+	for i in random.sample(options, args.num): print i
+	
+sys.exit(0)
