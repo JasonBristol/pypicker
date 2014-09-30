43c2b9d3f0d337e18eda23767b48ecdd3714ffe5
Jason Bristol <gethsemane369@gmail.com>
2014-09-30 15:15:20 -0400

    minor formatting

--- pypicker.py
+++ pypicker.py
@@ -7,16 +7,16 @@ import argparse, random, sys
 from pypicker_exceptions import *
 
 parser = argparse.ArgumentParser(
-	description='Randomly pick n items from a list', 
-	epilog='Created by Jason Bristol <Gethsemane369@gmail.com>')
+  description='Randomly pick n items from a list', 
+  epilog='Created by Jason Bristol <Gethsemane369@gmail.com>')
 
 parser.add_argument('n',   metavar='N', type=int, nargs='?', default=1, help='number of items to pick (default 1)')
-parser.add_argument('i',   metavar='I', type=str,                				help='input file of items to choose from')
-parser.add_argument('-ng',              type=int,   	     	 default=1, help='number of result groups to generate (default 1)')
-parser.add_argument('-o',               type=str,                				help='output file to write results to')
-parser.add_argument('-j',               type=str,                				help='join results using specified delimeter')
-parser.add_argument('-v',               action='store_true',     				help='verbose output')
-parser.add_argument('-u',								action='store_true',						help='force uniqueness across result groups')
+parser.add_argument('i',   metavar='I', type=str,                       help='input file of items to choose from')
+parser.add_argument('-ng',              type=int,            default=1, help='number of result groups to generate (default 1)')
+parser.add_argument('-o',               type=str,                       help='output file to write results to')
+parser.add_argument('-j',               type=str,                       help='join results using specified delimeter')
+parser.add_argument('-v',               action='store_true',            help='verbose output')
+parser.add_argument('-u',               action='store_true',            help='force uniqueness across result groups')
 
 args = parser.parse_args()
 
@@ -27,35 +27,35 @@ sampleSize   = 0
 ''' Validates and orchestrates result picks '''
 def pick(n, num_groups, input_file, output_file, verbose, join, unique_groups):
 
-	if n not in range(0, len(options)): raise BoundsException("n out of bounds, please select a number within 1 <= n <= total items")
+  if n not in range(0, len(options)): raise BoundsException("n out of bounds, please select a number within 1 <= n <= total items")
 
-	sampleSize = len(options)
+  sampleSize = len(options)
 
-	for i in range(0, num_groups):
-		results = random.sample(options, n)
+  for i in range(0, num_groups):
+    results = random.sample(options, n)
 
-		if unique_groups:
-			if (n * num_groups) > sampleSize: raise ResultSetException("Requested result set is larger than sample size")
-			else:
-				for result in results: options[:] = [x for x in options if x != result]
-		
-		if verbose: resultString.append("Picking {} individual(s) out of {}:".format(n, len(options) + n))
-		resultString.append("\n".join(results) if join == None else "{} ".format(join).join(results))
+    if unique_groups:
+      if (n * num_groups) > sampleSize: raise ResultSetException("Requested result set is larger than sample size")
+      else:
+        for result in results: options[:] = [x for x in options if x != result]
+    
+    if verbose: resultString.append("Picking {} individual(s) out of {}:".format(n, len(options) + n))
+    resultString.append("\n".join(results) if join == None else "{} ".format(join).join(results))
 
-	if output_file == None: print '\n\n'.join(resultString)
-	else:
-		try:
-			with open(output_file,'w') as f: f.write('\n\n'.join(resultString))
-		except:  raise OutputException(sys.exc_info[0])
-		finally: f.close
+  if output_file == None: print '\n\n'.join(resultString)
+  else:
+    try:
+      with open(output_file,'w') as f: f.write('\n\n'.join(resultString))
+    except:  raise OutputException(sys.exc_info[0])
+    finally: f.close
 
-''' Main Method '''			
+''' Main Method '''     
 if __name__ == "__main__":
-	try:
-		options = [line.strip() for line in open(args.i)]
-		pick(args.n, args.ng, args.i, args.o, args.v, args.j, args.u)
-	except BoundsException      as e: print "\n{}: {}\n".format(type(e).__name__, e.msg)
-	except OutputException 	    as e: print "\n{}: {}\n".format(type(e).__name__, e.msg)
-	except ResultSetException   as e: print "\n{}: {}\n".format(type(e).__name__, e.msg)
-	except                          : print "\n{}: {}\n".format("Unexpected error", sys.exc_info()[0])
-	finally                         : sys.exit(0)
+  try:
+    options = [line.strip() for line in open(args.i)]
+    pick(args.n, args.ng, args.i, args.o, args.v, args.j, args.u)
+  except BoundsException      as e: print "\n{}: {}\n".format(type(e).__name__, e.msg)
+  except OutputException      as e: print "\n{}: {}\n".format(type(e).__name__, e.msg)
+  except ResultSetException   as e: print "\n{}: {}\n".format(type(e).__name__, e.msg)
+  except                          : print "\n{}: {}\n".format("Unexpected error", sys.exc_info()[0])
+  finally                         : sys.exit(0)
