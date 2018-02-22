e04ea84388b2f613bd434be2ca4998e3675e13c4
Jason Bristol <json.bristol@gmail.com>
2018-02-22 13:19:52 -0500

    v1.0 Release: - Removed sampling algorithms in favor of just a simple random sample - Adjusted input parameters to be more concise - Added the ability to define a list to choose from on the command line - Updated readme - Upgraded from python 2.7 to python 3.6 - Minor optimizations and bug fixes

--- README.md
+++ README.md
@@ -4,51 +4,48 @@ pypicker
 A command line random picker for python.
 
 ```
-usage: pypicker.py [-h] [-ng NG] [-o O] [-j J] [-v] [-u]
-                   [-sa [{SRS,SYS,CLS,STS}]]
-                   [N] I
-
-Randomly pick n items from a list
-
+usage: pypicker.py [-h] [-i PATH] [-g INTEGER] [-o PATH] [-j DELIMITER] [-v]
+                   [-u] [--version]
+                   [SAMPLE SIZE] [SAMPLE GROUP [SAMPLE GROUP ...]]
+                   
+Randomly pick items from a list
+ 
 positional arguments:
-  N                     number of items to pick (default 1)
-  I                     input file of items to choose from
-
+  SAMPLE SIZE    number of items to pick (default: 1)
+  SAMPLE GROUP   list of items to choose from.
+  
 optional arguments:
-  -h, --help            show this help message and exit
-  -ng NG                number of result groups to generate (default 1)
-  -o O                  output file to write results to
-  -j J                  join results using specified delimeter
-  -v                    verbose output
-  -u                    force uniqueness across result groups
-  -sa [{SRS,SYS,CLS,STS}]
-                        specifies the sampling algorithm to use (default SRS)
-
-Created by Jason Bristol <JasonB@my.ccsu.edu>
+  -h, --help     show this help message and exit
+  -i PATH        input file of items to choose from
+  -g INTEGER     number of result groups to generate (default: 1)
+  -o PATH        output file to write results to
+  -j DELIMITER   join results using specified delimiter
+  -v, --verbose  verbose output
+  -u, --unique   force uniqueness across result groups
+  --version      show program's version number and exit
+  
+Created by Jason Bristol <json.bristol@gmail.com>
 ```
+
+**Randomly pick 2 items from a list**
+
+```$ python pypicker.py 2 a b c d```
+
+>c<br/>
+a<br/>
+
 ========
 
-**Randomly pick an item from a list**
+**Randomly pick an item from a file**
 
-```$ python pypicker.py test_input.txt```
+```$ python pypicker.py -i test_input.txt```
 >Yolanda Holland
-
-========
-
-**Randomly pick 4 items from a list**
-
-```$ python pypicker.py 4 test_input.txt```
-
->Trevor Goodman<br/>
-Francisco Williams<br/>
-Jessica Mitchell<br/>
-Herbert Ball
 
 ========
 
 **Randomly pick 4 groups of 4 from a list**
 
-```$ python pypicker.py 4 test_input.txt -ng 4 -v```
+```$ python pypicker.py 4 -i test_input.txt -g 4 -v```
 
 >Picking 4 item(s) out of 29:<br/>
 Faith Boone<br/>
@@ -78,7 +75,7 @@ Wilson Marshall<br/>
 
 **Randomly pick 4 unique groups of 4 from a list**
 
-```$ python pypicker.py 4 test_input.txt -ng -v -u```
+```$ python pypicker.py 4 -i test_input.txt -g -v -u```
 
 >Picking 4 item(s) out of 25:<br/>
 Danielle Gill<br/>
@@ -108,7 +105,7 @@ Betty Lane<br/>
 
 **Write output to a file**
 
-```$ python pypicker.py 4 test_input.txt -o test_ouptut.txt```
+```$ python pypicker.py 4 -i test_input.txt -o test_ouptut.txt```
 
 *test_output.txt*
 
@@ -121,7 +118,7 @@ Phillip Bailey
 
 **Join the results and seperate by a delimeter**
 
-```$ python pypicker.py 4 test_input.txt -j ', '```
+```$ python pypicker.py 4 -i test_input.txt -j ', '```
 
 >Shirley Joseph, Brandon Rice, Ellis Jennings, Anita Larson
 
--- pypicker.py
+++ pypicker.py
@@ -1,127 +1,142 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-__author__  = 'Jason Bristol'
-__version__ = '2.1'
+__version__ = 1.0
 
-import argparse, random, sys, codecs, itertools
-from pypicker_exceptions import *
+import argparse
+import random
 
 parser = argparse.ArgumentParser(
-  description='Randomly pick n items from a list', 
-  epilog='Created by Jason Bristol')
+    description='Randomly pick items from a list',
+    epilog='Created by Jason Bristol <json.bristol@gmail.com>')
 
-parser.add_argument('n',   metavar='N', type=int, nargs=1,                           default=1,     help='number of items to pick (default 1)')
-parser.add_argument('i',   metavar='I', type=str, nargs=1,                                          help='input file of items to choose from')
-parser.add_argument('-ng',              type=int,                                    default=1,     help='number of result groups to generate (default 1)')
-parser.add_argument('-o',               type=str,                                                   help='output file to write results to')
-parser.add_argument('-j',               type=str,                                                   help='join results using specified delimeter')
-parser.add_argument('-v',               action='store_true',                                        help='verbose output')
-parser.add_argument('-u',               action='store_true',                                        help='force uniqueness across result groups')
-parser.add_argument('-sa',              type=str, choices=('SRS','SYS','CLS','STS'), default='SRS', help='specifies the sampling algorithm to use (default SRS)')
+parser.add_argument('n',
+                    metavar='SAMPLE SIZE',
+                    nargs='?',
+                    type=int,
+                    default=1,
+                    help="number of items to pick (default: %(default)s)")
+parser.add_argument('list',
+                    metavar='SAMPLE GROUP',
+                    nargs='*',
+                    type=str,
+                    default=[],
+                    help="list of items to choose from.")
+parser.add_argument('-i',
+                    metavar='PATH',
+                    type=argparse.FileType('r'),
+                    help="input file of items to choose from")
+parser.add_argument('-g',
+                    metavar='INTEGER',
+                    type=int,
+                    default=1,
+                    help="number of result groups to generate (default: %(default)s)")
+parser.add_argument('-o',
+                    metavar='PATH',
+                    type=argparse.FileType('w'),
+                    help="output file to write results to")
+parser.add_argument('-j',
+                    metavar='DELIMITER',
+                    type=str,
+                    help="join results using specified delimiter")
+parser.add_argument('-v', '--verbose',
+                    action='store_true',
+                    help="verbose output")
+parser.add_argument('-u', '--unique',
+                    action='store_true',
+                    help="force uniqueness across result groups")
+parser.add_argument('--version',
+                    action='version',
+                    version='%(prog)s {version}'.format(version=__version__))
 
 args = parser.parse_args()
 
-options      = []      # Items to choose from
-resultString = []      # Array of result strings
-sampleSize   = [0, 0]  # [<num_picked>, <num_total>]
 
-# Functions
+class PyPickerException(Exception):
+    """ This is the base PyPicker error class """
+    pass
 
-# SRS
-#
-# A simple random sample is a sample in which every member of the population has an equal chance of being chosen.
-def SimpleRandomSample(n):
-  return random.sample(options, n)
 
-# SYS
-#
-# In systematic sampling, every kth member of the population is chosen for the sample, with value of k 
-# being approximately N/n (N: # in population; n: # of samples) '''
-def SystematicSample(n):
-  k = len(options) / n
-  results = []
+class BoundsException(PyPickerException):
+    """ This error occurs when the n argument is out of bounds """
 
-  for i in xrange(0, n):
-    results.append(options[k * (i + 1)])
+    def __init__(self, msg):
+        self.msg = str(msg)
 
-  return results
 
-# CLS
-#
-# A cluster sample is a simple random sample of groups, or clusters, of a population. Each member of the 
-# chosen clusters would be part of the final sample.
-def ClusterSample(n):
-  # r = random.randint(2, len(options))
-  # d = random.randint(1, (len(options) / 4))
-  # clusters = []
-  # results  = []
+class OutputException(PyPickerException):
+    """ This error occurs when the output file cannot be written to """
 
-  # for i in xrange(0, r):
-  #   clusters.append(split_list(options))
+    def __init__(self, msg):
+        self.msg = str(msg)
 
-  # for _ in itertools.repeat(None, d):
-  #   random.shuffle(clusters)
-  #   clusters.pop()
 
-  # for i in xrange(0, n):
-  #   cluster = random.randint(0, len(clusters))
-  #   element = random.randint(0, len(clusters[cluster]))
-  #   results.append(clusters[cluster][element])
+class ResultSetException(PyPickerException):
+    """ This error occurs when the requested result set is greater than the sample size """
 
-  # return results
-  return
+    def __init__(self, msg):
+        self.msg = str(msg)
 
-# STS
-#
-# A stratified sample is obtained by dividing the population into mutually exclusive groups, or strata, 
-# and randomly sampling from each of these groups.
-def StratifiedSample(n):
-  return
 
-# Helper function to split a list in half
-def split_list(a_list):
-  half = len(a_list) / 2
-  return a_list[:half], a_list[half:]
+def SimpleRandomSample(sample_set, sample_size):
+    """
+    A simple random sample is a sample in which every member of the population has an equal chance of being chosen.
+    :param sample_set: set of values to select from
+    :param sample_size: number of values to select
+    :return: result sample
+    """
+    return random.sample(sample_set, sample_size)
 
-# Validates and orchestrates result picks
-def pick(n, num_groups, input_file, output_file, verbose, join, unique_groups, algorithm):
 
-  if n not in xrange(0, len(options)): raise BoundsException("n out of bounds, please select a number within 1 <= n <= total items")
+def pick(n, num_groups, sample_set, output_file, verbose, join, unique_groups):
+    """
+    Validates and orchestrates result picks
+    :param n: number of values to pick
+    :param num_groups: number of samples to pick
+    :param sample_set: set of values to choose from
+    :param output_file: path to output file
+    :param verbose: set verbosity
+    :param join: join the result with a delimiter
+    :param unique_groups: set uniqueness
+    :return:
+    """
+    resultString = []  # Array of result strings
+    sampleSize = [0, 0]  # [<num_picked>, <num_total>]
 
-  sampleSize[1] = len(options)
+    if n not in range(0, len(sample_set) + 1):
+        raise BoundsException("n out of bounds, please select a number within 1 <= n <= total items")
 
-  for i in xrange(0, num_groups):
-    if   algorithm == 'SRS': results = SimpleRandomSample(n)
-    elif algorithm == 'SYS': results = SystematicSample(n)
-    elif algorithm == 'CLS': results = ClusterSample(n)
-    elif algorithm == 'STS': results = StratifiedSample(n)
+    sampleSize[1] = len(sample_set)
 
-    sampleStr = (sampleSize[1] - sampleSize[0]) if unique_groups else sampleSize[1]
+    for i in range(0, num_groups):
+        results = SimpleRandomSample(sample_set, n)
 
-    if unique_groups:
-      if (n * num_groups) > sampleSize[1]: raise ResultSetException("Requested result set is larger than sample size")
-      else:
-        for result in results: options[:] = [x for x in options if x != result]
-    
-    if verbose: resultString.append("Picking {} item(s) out of {}:".format(n, sampleStr))
-    resultString.append("\n".join(results) if join == None else "{}".format(join).join(results))
+        sampleStr = (sampleSize[1] - sampleSize[0]) if unique_groups else sampleSize[1]
 
-    sampleSize[0] += n
+        if unique_groups:
+            if (n * num_groups) > sampleSize[1]:
+                raise ResultSetException("Requested result set is larger than sample size")
+            else:
+                for result in results:
+                    sample_set[:] = [x for x in sample_set if x != result]
 
-  if output_file == None: print "\n\n".join(resultString)
-  else:
-    try:
-      with codecs.open(output_file,'w', 'utf-8') as f: f.write("\n\n".join(resultString))
-    except:  raise OutputException(sys.exc_info[1])
-    finally: f.close
+        if verbose:
+            resultString.append("Picking {} item(s) out of {}:".format(n, sampleStr))
 
-# Main Method     
+        resultString.append("\n".join(results) if join is None else "{}".format(join).join(results))
+
+        sampleSize[0] += n
+
+    if output_file == None:
+        print("\n\n".join(resultString))
+    else:
+        try:
+            output_file.write("\n\n".join(resultString))
+        except Exception as e:
+            raise OutputException(e)
+
+
 if __name__ == "__main__":
-  try:
-    options = [line.strip() for line in codecs.open(args.i, 'r', 'utf-8')]
-    random.shuffle(options)
-    pick(args.n, args.ng, args.i, args.o, args.v, args.j, args.u, args.sa)
-  except BoundsException      as e: print "\n{}: {}\n".format(type(e).__name__, e.msg)
-  except OutputException      as e: print "\n{}: {}\n".format(type(e).__name__, e.msg)
-  except ResultSetException   as e: print "\n{}: {}\n".format(type(e).__name__, e.msg)
-  except                          : print "\n{}: {}\n".format("Unexpected error", sys.exc_info()[1])
+    sample_set = args.list if args.i is None else [line.strip() for line in args.i.readlines()]
+    random.shuffle(sample_set)
+    pick(args.n, args.g, sample_set, args.o, args.verbose, args.j, args.unique)
